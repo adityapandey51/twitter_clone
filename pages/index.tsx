1d9de3cea30ce0f2447a8273e883229c4366c07d
Aditya Pandey <adityapandey3082004@gmail.com>
2024-02-29 23:13:50 +0530

    completed tweet modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
 import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
 import { SlOptions } from "react-icons/sl";
+import { FaImage } from "react-icons/fa6";
 import FeedCard from "@/components/FeedCard";
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import toast from "react-hot-toast";
@@ -54,6 +55,14 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
 export default function Home() {
   const {user}=useCurrentUser()
   const queryClient=useQueryClient()
+  
+  const handleOnClickImage=useCallback(()=>{
+    const input=document.createElement('input')
+    input.setAttribute('type','file');
+    input.setAttribute('accept','image/*')
+    input.click()
+  },[]);
+  
   const handleLoginWithGoogle=useCallback(async(cred:CredentialResponse)=>{
       
       const googleToken=cred.credential;
@@ -122,6 +131,34 @@ export default function Home() {
           )}
         </div>
         <div className="col-span-5 border-r-[0.25px] border-l-[0.25px] h-screen overflow-y-scroll no-scrollbar border-gray-700">
+          <div className="border border-r-0 border-l-0 border-b-0 border-gray-700 p-4 hover:bg-slate-900 transition-all cursor-pointer">
+            <div className="grid grid-cols-12 gap-3">
+              <div className="col-span-1">
+                {user?.profileImage && (
+                  <Image
+                    className="rounded-full"
+                    src={user?.profileImage}
+                    height={50}
+                    width={50}
+                    alt="User_Image"
+                  />
+                )}
+              </div>
+              <div className="col-span-11">
+                <textarea
+                  className="bg-transparent w-full text-xl px-2 border-b border-slate-400"
+                  placeholder="What's happening?"
+                  rows={4}
+                ></textarea>
+                <div className="flex justify-between items-center mt-[6px] px-1">
+                  <FaImage onClick={handleOnClickImage} className="text-xl"></FaImage>
+                  <button className="bg-[#1d9bf0] font-semibold text-lg py-1 px-4 rounded-full">
+                    Tweet
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <FeedCard />
           <FeedCard />
           <FeedCard />
@@ -129,15 +166,17 @@ export default function Home() {
           <FeedCard />
         </div>
         <div className="col-span-3 px-8 py-4">
-          {!user && <div className="h-[150px] w-[400px] rounded-lg bg-slate-400 px-4 py-4">
-            <h1 className="mb-[10px] text-2xl">New to Twitter?</h1>
-            <GoogleLogin
-              onSuccess={handleLoginWithGoogle}
-              onError={() => {
-                console.log("Login Failed");
-              }}
-            />
-          </div>}
+          {!user && (
+            <div className="h-[150px] w-[400px] rounded-lg bg-slate-400 px-4 py-4">
+              <h1 className="mb-[10px] text-2xl">New to Twitter?</h1>
+              <GoogleLogin
+                onSuccess={handleLoginWithGoogle}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
